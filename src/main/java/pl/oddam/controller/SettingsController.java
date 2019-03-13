c30092540678bbd1713de9e0b4ccd1c423373eba
Cutter72 <drelich_pawel@o2.pl>
2019-03-13 19:46:25 +0100

    Edit personal data work

--- src/main/java/pl/oddam/controller/SettingsController.java
+++ src/main/java/pl/oddam/controller/SettingsController.java
@@ -4,12 +4,21 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.oddam.model.CurrentUser;
+import pl.oddam.repository.UserRepository;
 
 @Controller
 @RequestMapping("/user/settings")
 public class SettingsController {
+    private final UserRepository userRepository;
+
+    public SettingsController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("")
     public String userSettings(@AuthenticationPrincipal CurrentUser customUser, Model model) {
         model.addAttribute("user", customUser.getUser());
@@ -23,5 +32,12 @@ public class SettingsController {
     public String editPersonalData(@AuthenticationPrincipal CurrentUser customUser, Model model) {
         model.addAttribute("user", customUser.getUser());
         return "user/editPersonalData";
+    }
+    @PostMapping("/edit-personal-data")
+    public String editPersonalDataSaveToDb(@AuthenticationPrincipal CurrentUser customUser, @RequestParam String firstName, @RequestParam String lastName) {
+        customUser.getUser().setFirstName(firstName);
+        customUser.getUser().setLastName(lastName);
+        userRepository.save(customUser.getUser());
+        return "redirect:/user/profile";
     }
 }

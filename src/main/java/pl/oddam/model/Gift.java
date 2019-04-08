239485bb61a45ba60d28be621484a6f5c32b695f
Cutter72 <drelich_pawel@o2.pl>
2019-04-08 22:25:21 +0200

    Gift Entity created

---
+++ src/main/java/pl/oddam/model/Gift.java
@@ -0,0 +1,29 @@
+package pl.oddam.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Gift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Organization organization;
+    private Integer bags;
+    private String street;
+    private String city;
+    private String postCode;
+    private String callNumber;
+    private LocalDateTime created;
+    private Date preferredDateOfCollection;
+    private Time preferredTimeOfCollection;
+    private String courierNote;
+    private LocalDateTime timeCollected;
+    private boolean collected;
+}

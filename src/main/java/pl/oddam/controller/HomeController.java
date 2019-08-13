f457fc3f0c904aaae14e301f38480a328ed5359e
Cutter72 <drelich_pawel@o2.pl>
2019-08-13 20:49:21 +0200

    homepage counters added

--- src/main/java/pl/oddam/controller/HomeController.java
+++ src/main/java/pl/oddam/controller/HomeController.java
@@ -3,15 +3,28 @@ package pl.oddam.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.oddam.repository.GiftRepository;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
+    private final GiftRepository giftRepository;
+
+    public HomeController(GiftRepository giftRepository) {
+        this.giftRepository = giftRepository;
+    }
 
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        int bagsCount = giftRepository.countBags();
+        int supportedOrganizations = giftRepository.countSupportedOrganizations();
+        int giftsCount = giftRepository.countAll();
+        model.addAttribute("bagsCount", bagsCount);
+        model.addAttribute("supportedOrganizations", supportedOrganizations);
+        model.addAttribute("giftsCount", giftsCount);
         return "home";
     }
 
--- src/main/java/pl/oddam/repository/GiftRepository.java
+++ src/main/java/pl/oddam/repository/GiftRepository.java
@@ -1,6 +1,7 @@
 package pl.oddam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.oddam.model.Gift;
 import pl.oddam.model.User;
@@ -10,4 +11,10 @@ import java.util.List;
 @Repository
 public interface GiftRepository extends JpaRepository<Gift, Long> {
     List<Gift> findAllByUser(User user);
+    @Query(nativeQuery = true, value = "SELECT SUM(bags) FROM gift;")
+    Integer countBags();
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) from (select gift.organization_id from gift group by organization_id) as supportedOrganizations;")
+    Integer countSupportedOrganizations();
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) from gift;")
+    Integer countAll();
 }
